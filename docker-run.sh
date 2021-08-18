4507a674efe6e446cd6e3eeb2b11e9db1028214f
ekasyan <j.kasyan86@gmail.com>
2021-08-18 22:55:02 +0300

    - fixed docker + consul;

--- docker-run.sh
+++ docker-run.sh
@@ -2,5 +2,5 @@
 
 mvn clean && mvn package
 docker build --no-cache -t com.example/entry entry/.
-docker-compose -f docker-compose.app.yml up
-#docker run -ti --network  wallet_local --rm -p 8601:8601 com.example/entry
+docker build --no-cache -t com.example/task task/.
+docker-compose -f docker-compose.app.yml up --scale task=2
